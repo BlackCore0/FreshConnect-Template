644038f2546b0bb37d4fb4abce67490fa3a4f426
William <429581@student.saxion.nl>
2023-05-19 10:22:22 +0200

    Initial commit

---
+++ src/test/java/com/mari/freshconnect/FreshconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mari.freshconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
